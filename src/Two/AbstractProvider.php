be517684c310e2abdbf13187790254323750e91c
Taylor Otwell <taylorotwell@gmail.com>
2015-02-24 14:20:00 -0600

    rename method.

--- src/Two/AbstractProvider.php
+++ src/Two/AbstractProvider.php
@@ -122,7 +122,22 @@ abstract class AbstractProvider implements ProviderContract
     {
         $session = $this->request->getSession();
 
-        return $url.'?'.http_build_query($this->getCodeRequestParameters($state), '', '&', $this->encodingType);
+        return $url.'?'.http_build_query($this->getCodeFields($state), '', '&', $this->encodingType);
+    }
+
+    /**
+     * Get the GET parameters for the code request.
+     *
+     * @param  string  $state
+     * @return array
+     */
+    protected function getCodeFields($state)
+    {
+        return [
+            'client_id' => $this->clientId, 'redirect_uri' => $this->redirectUrl,
+            'scope' => $this->formatScopes($this->scopes, $this->scopeSeparator), 'state' => $state,
+            'response_type' => 'code',
+        ];
     }
 
     /**
@@ -179,21 +194,6 @@ abstract class AbstractProvider implements ProviderContract
         ]);
 
         return $this->parseAccessToken($response->getBody());
-    }
-
-    /**
-     * Get the GET parameters for the auth_code request.
-     *
-     * @param  string  $state
-     * @return array
-     */
-    protected function getCodeRequestParameters($state)
-    {
-        return [
-            'client_id' => $this->clientId, 'redirect_uri' => $this->redirectUrl,
-            'scope' => $this->formatScopes($this->scopes, $this->scopeSeparator), 'state' => $state,
-            'response_type' => 'code',
-        ];
     }
 
     /**
