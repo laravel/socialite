19cc7288826f2adbcdd63ba68a4f640b5e77bc83
Adam Buchen <adam.buchen@yahoo.com>
2015-03-02 23:34:47 -0800

    Override of user method in Twitter Provider.
    This is so that the urls property that comes back from the OAuth library can be accessed in the getOffset() method. Necessary to get HTTPS avatar URL

--- src/One/TwitterProvider.php
+++ src/One/TwitterProvider.php
@@ -2,6 +2,29 @@
 
 class TwitterProvider extends AbstractProvider
 {
+    /**
+     * Get the User instance for the authenticated user.
+     * Overridden to provide access to the urls property that comes back from the
+     * OAuth library
+     *
+     * @return \Laravel\Socialite\One\User
+     */
+    public function user()
+    {
+        if (! $this->hasNecessaryVerifier()) {
+            throw new \InvalidArgumentException("Invalid request. Missing OAuth verifier.");
+        }
 
-    //
+        $user = $this->server->getUserDetails($token = $this->getToken());
+
+        $instance = (new User)->setRaw(array_merge($user->extra, $user->urls))
+                ->setToken($token->getIdentifier(), $token->getSecret());
+
+        return $instance->map([
+            'id' => $user->uid, 'nickname' => $user->nickname,
+            'name' => $user->name, 'email' => $user->email, 'avatar' => $user->imageUrl,
+        ]);
+    }
+
 }
+
