ee1455713012826b4e2d01dac21a205e760e045a
Michael Palmer <mail@michaelpalmer.de>
2015-03-30 11:38:54 +0200

    getting email fix for github users
    after oauth there isn't email provided in all cases. this fix asked the github api for all user emails and stores the primary to the oauth users array

--- src/Two/GithubProvider.php
+++ src/Two/GithubProvider.php
@@ -33,6 +33,7 @@ class GithubProvider extends AbstractProvider implements ProviderInterface
      */
     protected function getUserByToken($token)
     {
+        $this->token = $token;
         $response = $this->getHttpClient()->get('https://api.github.com/user?access_token='.$token, [
             'headers' => [
                 'Accept' => 'application/vnd.github.v3+json',
@@ -41,12 +42,35 @@ class GithubProvider extends AbstractProvider implements ProviderInterface
 
         return json_decode($response->getBody(), true);
     }
-
+    
+    /**
+     * {@inheritdoc}
+     */
+    private function getUserEmailByToken($token)
+    {
+        $email = null;
+		$response = $this->getHttpClient()->get('https://api.github.com/user/emails?access_token='.$token, [
+				'headers' => [
+						'Accept' => 'application/vnd.github.v3+json',
+				],
+		]);
+		$userEmails = json_decode($response->getBody(), true);
+        foreach($userEmails AS $key => $array) {
+			if($array['primary'] == true && $array['verified'] == true) {
+				$email = $array['email'];
+			}
+		}
+		return $email;
+    }
+    
     /**
      * {@inheritdoc}
      */
     protected function mapUserToObject(array $user)
     {
+        if(is_null(array_get($user, 'email'))) {
+            $user['email'] = $this->getUserEmailByToken($this->token);
+        }
         return (new User)->setRaw($user)->map([
             'id' => $user['id'], 'nickname' => $user['login'], 'name' => array_get($user, 'name'),
             'email' => array_get($user, 'email'), 'avatar' => $user['avatar_url'],
