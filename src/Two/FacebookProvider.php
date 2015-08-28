f746424b8175a0794275d3ed45d94ac5140f35d4
José Emmanuel Lozoya <jose.emmanuel.lozoya@gmail.com>
2015-08-28 02:58:59 -0500

    Send appsecret_proof on getUserByToken method.

--- src/Two/FacebookProvider.php
+++ src/Two/FacebookProvider.php
@@ -85,7 +85,8 @@ class FacebookProvider extends AbstractProvider implements ProviderInterface
      */
     protected function getUserByToken($token)
     {
-        $response = $this->getHttpClient()->get($this->graphUrl.'/'.$this->version.'/me?access_token='.$token.'&fields='.implode(',', $this->fields), [
+        $appsecret_proof = hash_hmac('sha256', $token, $this->clientSecret);
+        $response = $this->getHttpClient()->get($this->graphUrl.'/'.$this->version.'/me?access_token='.$token.'&appsecret_proof='.$appsecret_proof.'&fields='.implode(',', $this->fields), [
             'headers' => [
                 'Accept' => 'application/json',
             ],
