d6d35f98515add95b6fedf1a7503936f81038cdd
Victor Michnowicz <victor@forexfactory.com>
2015-09-22 16:10:01 -0400

    Allow for custom config

--- src/SocialiteManager.php
+++ src/SocialiteManager.php
@@ -129,11 +129,20 @@ class SocialiteManager extends Manager implements Contracts\Factory
      */
     public function formatConfig(array $config)
     {
-        return [
-            'identifier' => $config['client_id'],
-            'secret' => $config['client_secret'],
-            'callback_uri' => $config['redirect'],
-        ];
+        $return = array();
+        $custom = array('client_id' => 'identifier', 'client_secret' => 'secret', 'redirect' => 'callback_uri');
+
+        foreach ($config as $key => $value) {
+            // If there is a custom mapping for this config
+            if (isset($custom[$key])) {
+                $return[ $custom[$key] ] = $value;
+            }
+            else {
+                $return[$key] = $value;
+            }
+        }
+
+        return $return;
     }
 
     /**
