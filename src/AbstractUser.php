9240d9f498457f8d27db6865c3dc9f8e22c0ba9c
Taylor Otwell <taylorotwell@gmail.com>
2016-01-13 08:18:59 -0600

    move method order

--- src/AbstractUser.php
+++ src/AbstractUser.php
@@ -92,6 +92,16 @@ abstract class AbstractUser implements ArrayAccess, Contracts\User
     }
 
     /**
+     * Get the raw user array.
+     *
+     * @return array
+     */
+    public function getRaw()
+    {
+        return $this->user;
+    }
+
+    /**
      * Set the raw user array from the provider.
      *
      * @param  array  $user
@@ -102,16 +112,6 @@ abstract class AbstractUser implements ArrayAccess, Contracts\User
         $this->user = $user;
 
         return $this;
-    }
-
-    /**
-     * Get the raw user array
-     *
-     * @return array
-     */
-    public function getRaw()
-    {
-        return $this->user;
     }
 
     /**
