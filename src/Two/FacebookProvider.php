6904c08dc5346e4c56d036787c12a03d390440db
George <info@geodeveloper.net>
2016-09-21 13:09:30 +0200

    Fix styling (StyleCI)

--- src/Two/FacebookProvider.php
+++ src/Two/FacebookProvider.php
@@ -171,7 +171,7 @@ class FacebookProvider extends AbstractProvider implements ProviderInterface
     public function reRequest()
     {
         $this->reRequest = true;
-        
+
         return $this;
     }
 }
