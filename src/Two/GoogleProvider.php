57ce28fb06ad761603a88b8d1c40f68e3644a6ec
Sergei Chukhan <chuhan.sergey@gmail.com>
2016-10-08 20:16:48 +0300

    Added "avatar_original" which returns a full size photo instead of 50x50 for Google provider.

--- src/Two/GoogleProvider.php
+++ src/Two/GoogleProvider.php
@@ -77,8 +77,12 @@ class GoogleProvider extends AbstractProvider implements ProviderInterface
     protected function mapUserToObject(array $user)
     {
         return (new User)->setRaw($user)->map([
-            'id' => $user['id'], 'nickname' => Arr::get($user, 'nickname'), 'name' => $user['displayName'],
-            'email' => $user['emails'][0]['value'], 'avatar' => Arr::get($user, 'image')['url'],
+            'id'              => $user['id'],
+            'nickname'        => Arr::get($user, 'nickname'), 
+            'name'            => $user['displayName'],
+            'email'           => $user['emails'][0]['value'],
+            'avatar'          => Arr::get($user, 'image')['url'],
+            'avatar_original' => preg_replace('/\?sz=([0-9]+)/', '', Arr::get($user, 'image')['url']),
         ]);
     }
 }
