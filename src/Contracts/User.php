ed4de3541c4b115e5da54389abe35e6c6a59d930
Bimochan Shrestha <bmochan@gmail.com>
2017-03-05 18:21:52 +0545

    styleci correction2

--- src/Contracts/User.php
+++ src/Contracts/User.php
@@ -38,6 +38,11 @@ interface User
      * @return string
      */
     public function getAvatar();
-    
+
+    /**
+     * Get the gender of the user.
+     *
+     * @return string
+     */
     public function getGender();
 }
