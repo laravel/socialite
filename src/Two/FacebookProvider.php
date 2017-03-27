b6d40c126d2eda20fd48d4582ae06a1c1f5f880d
Tom Sisk <tom.sisk@gmail.com>
2017-03-27 13:21:13 -0500

    Fixing access token parsing
    Specifying version when requesting access token. v2.2 of Facebook API (last version to use query strings here) is no longer available.

--- src/Two/FacebookProvider.php
+++ src/Two/FacebookProvider.php
@@ -62,7 +62,7 @@ class FacebookProvider extends AbstractProvider implements ProviderInterface
      */
     protected function getTokenUrl()
     {
-        return $this->graphUrl.'/oauth/access_token';
+        return $this->graphUrl.'/'.$this->version.'/oauth/access_token';
     }
 
     /**
@@ -76,9 +76,7 @@ class FacebookProvider extends AbstractProvider implements ProviderInterface
             $postKey => $this->getTokenFields($code),
         ]);
 
-        $data = [];
-
-        parse_str($response->getBody(), $data);
+        $data = json_decode($response->getBody(), true);
 
         return Arr::add($data, 'expires_in', Arr::pull($data, 'expires'));
     }
