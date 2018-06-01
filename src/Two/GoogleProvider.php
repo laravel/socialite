b5f465847b1d637efa86bbfe2fc1c9d2bd12f60f
Taylor Otwell <taylor@laravel.com>
2018-06-01 10:06:47 -0500

    use array get

--- src/Two/GoogleProvider.php
+++ src/Two/GoogleProvider.php
@@ -78,7 +78,7 @@ class GoogleProvider extends AbstractProvider implements ProviderInterface
     {
         return (new User)->setRaw($user)->map([
             'id' => $user['id'], 'nickname' => Arr::get($user, 'nickname'), 'name' => $user['displayName'],
-            'email' => $user['emails'][0]['value'], 'avatar' => Arr::get($user, 'image')['url'],
+            'email' => Arr::get($user, 'emails.0.value'), 'avatar' => Arr::get($user, 'image')['url'],
             'avatar_original' => preg_replace('/\?sz=([0-9]+)/', '', Arr::get($user, 'image')['url']),
         ]);
     }
