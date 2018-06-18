4b5cc97c8d480d072927129bcfe4bce40cc785e8
Johan Krijt <johan@goedemiddag.nl>
2018-06-18 12:20:23 +0200

    Bump Graph API version to v3.0

--- src/Two/FacebookProvider.php
+++ src/Two/FacebookProvider.php
@@ -19,7 +19,7 @@ class FacebookProvider extends AbstractProvider implements ProviderInterface
      *
      * @var string
      */
-    protected $version = 'v2.9';
+    protected $version = 'v3.0';
 
     /**
      * The user fields being requested.
