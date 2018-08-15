24b3045d2a0d3a60fac5619432e9bb7a31a5db64
Chris Forrence <chrisforrence@users.noreply.github.com>
2018-08-15 17:26:12 -0400

    Changed isNewUser call to shouldBypassCache
    This fixes an issue where the isNewUser method was renamed to shouldBypassCache but not changed here.

--- src/One/TwitterProvider.php
+++ src/One/TwitterProvider.php
@@ -15,7 +15,7 @@ class TwitterProvider extends AbstractProvider
             throw new InvalidArgumentException('Invalid request. Missing OAuth verifier.');
         }
 
-        $user = $this->server->getUserDetails($token = $this->getToken(), $this->isNewUser($token->getIdentifier(), $token->getSecret()));
+        $user = $this->server->getUserDetails($token = $this->getToken(), $this->shouldBypassCache($token->getIdentifier(), $token->getSecret()));
 
         $extraDetails = [
             'location' => $user->location,
