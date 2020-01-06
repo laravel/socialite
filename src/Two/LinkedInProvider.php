123d0b596314bf95d8e4a94fc3db5efc025afd04
mertcagiran <tevfikmert.cagiran@mediaclick.com.tr>
2020-01-06 13:07:22 +0300

    linkedinprovider modified for linkedin api v2 instead of v1

--- src/Two/LinkedInProvider.php
+++ src/Two/LinkedInProvider.php
@@ -11,7 +11,7 @@ class LinkedInProvider extends AbstractProvider implements ProviderInterface
      *
      * @var array
      */
-    protected $scopes = ['r_basicprofile', 'r_emailaddress'];
+    protected $scopes = ['r_liteprofile', 'r_emailaddress'];
 
     /**
      * The separating character for the requested scopes.
@@ -19,17 +19,6 @@ class LinkedInProvider extends AbstractProvider implements ProviderInterface
      * @var string
      */
     protected $scopeSeparator = ' ';
-
-    /**
-     * The fields that are included in the profile.
-     *
-     * @var array
-     */
-    protected $fields = [
-        'id', 'first-name', 'last-name', 'formatted-name',
-        'email-address', 'headline', 'location', 'industry',
-        'public-profile-url', 'picture-url', 'picture-urls::(original)',
-    ];
 
     /**
      * {@inheritdoc}
@@ -50,7 +39,7 @@ class LinkedInProvider extends AbstractProvider implements ProviderInterface
     /**
      * Get the POST fields for the token request.
      *
-     * @param  string  $code
+     * @param string $code
      * @return array
      */
     protected function getTokenFields($code)
@@ -63,18 +52,50 @@ class LinkedInProvider extends AbstractProvider implements ProviderInterface
      */
     protected function getUserByToken($token)
     {
-        $fields = implode(',', $this->fields);
+        $basicProfile = $this->getBasicProfile($token);
+        $emailAddress = $this->getEmailAddress($token);
 
-        $url = 'https://api.linkedin.com/v1/people/~:('.$fields.')';
+        return array_merge($basicProfile, $emailAddress);
+    }
+
+    /**
+     * Get the basic profile fields for the user.
+     *
+     * @param string $token
+     * @return array
+     */
+    protected function getBasicProfile($token)
+    {
+        $url = 'https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))';
 
         $response = $this->getHttpClient()->get($url, [
             'headers' => [
-                'x-li-format' => 'json',
-                'Authorization' => 'Bearer '.$token,
+                'Authorization' => 'Bearer ' . $token,
+                'X-RestLi-Protocol-Version' => '2.0.0',
             ],
         ]);
 
-        return json_decode($response->getBody(), true);
+        return (array)json_decode($response->getBody(), true);
+    }
+
+    /**
+     * Get the email address for the user.
+     *
+     * @param string $token
+     * @return array
+     */
+    protected function getEmailAddress($token)
+    {
+        $url = 'https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))';
+
+        $response = $this->getHttpClient()->get($url, [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                'X-RestLi-Protocol-Version' => '2.0.0',
+            ],
+        ]);
+
+        return (array)Arr::get((array)json_decode($response->getBody(), true), 'elements.0.handle~');
     }
 
     /**
@@ -82,26 +103,27 @@ class LinkedInProvider extends AbstractProvider implements ProviderInterface
      */
     protected function mapUserToObject(array $user)
     {
+        $preferredLocale = Arr::get($user, 'firstName.preferredLocale.language') . '_' . Arr::get($user, 'firstName.preferredLocale.country');
+        $firstName = Arr::get($user, 'firstName.localized.' . $preferredLocale);
+        $lastName = Arr::get($user, 'lastName.localized.' . $preferredLocale);
+
+        $images = (array)Arr::get($user, 'profilePicture.displayImage~.elements', []);
+        $avatar = Arr::first(Arr::where($images, function ($image) {
+            return $image['data']['com.linkedin.digitalmedia.mediaartifact.StillImage']['storageSize']['width'] === 100;
+        }));
+        $originalAvatar = Arr::first(Arr::where($images, function ($image) {
+            return $image['data']['com.linkedin.digitalmedia.mediaartifact.StillImage']['storageSize']['width'] === 800;
+        }));
+
         return (new User)->setRaw($user)->map([
             'id' => $user['id'],
             'nickname' => null,
-            'name' => Arr::get($user, 'formattedName'),
+            'name' => $firstName . ' ' . $lastName,
+            'first_name' => $firstName,
+            'last_name' => $lastName,
             'email' => Arr::get($user, 'emailAddress'),
-            'avatar' => Arr::get($user, 'pictureUrl'),
-            'avatar_original' => Arr::get($user, 'pictureUrls.values.0'),
+            'avatar' => Arr::get($avatar, 'identifiers.0.identifier'),
+            'avatar_original' => Arr::get($originalAvatar, 'identifiers.0.identifier'),
         ]);
-    }
-
-    /**
-     * Set the user fields to request from LinkedIn.
-     *
-     * @param  array  $fields
-     * @return $this
-     */
-    public function fields(array $fields)
-    {
-        $this->fields = $fields;
-
-        return $this;
     }
 }
