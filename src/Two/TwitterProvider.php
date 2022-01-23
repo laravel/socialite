e58f915ec3068aaec69083fe4977ef7378f63822
dehbka <dehbkaa@gmail.com>
2022-01-23 11:57:49 +0200

    Add Twitter OAuth2 provider

---
+++ src/Two/TwitterProvider.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Laravel\Socialite\Two;
+
+use Illuminate\Support\Arr;
+
+class TwitterProvider extends AbstractProvider
+{
+    /**
+     * The scopes being requested.
+     *
+     * @var array
+     */
+    protected $scopes = ['users.read', 'tweet.read'];
+
+    /**
+     * Indicates if PKCE should be used.
+     *
+     * @var bool
+     */
+    protected $usesPKCE = true;
+
+    /**
+     * The separating character for the requested scopes.
+     *
+     * @var string
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://twitter.com/i/oauth2/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://api.twitter.com/2/oauth2/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://api.twitter.com/2/users/me', [
+            'headers' => ['Authorization' => 'Bearer '.$token],
+            'query' => ['user.fields' => 'profile_image_url'],
+        ]);
+
+        return Arr::get(json_decode($response->getBody(), true), 'data');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User)->setRaw($user)->map([
+            'id' => $user['id'],
+            'nickname' => $user['username'],
+            'name' => $user['name'],
+            'avatar' => $user['profile_image_url'],
+        ]);
+    }
+}
