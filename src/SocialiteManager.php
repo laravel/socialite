68afb03259b82d898c68196cbcacd48596a9dd72
Taylor Otwell <taylor@laravel.com>
2022-03-10 09:26:19 -0600

    allow oauth version in config

--- src/SocialiteManager.php
+++ src/SocialiteManager.php
@@ -116,6 +116,24 @@ class SocialiteManager extends Manager implements Contracts\Factory
     /**
      * Create an instance of the specified driver.
      *
+     * @return \Laravel\Socialite\One\AbstractProvider
+     */
+    protected function createTwitterDriver()
+    {
+        $config = $this->config->get('services.twitter');
+
+        if (($config['oauth'] ?? null) === 2) {
+            return $this->createTwitterOAuth2Driver();
+        }
+
+        return new TwitterProvider(
+            $this->container->make('request'), new TwitterServer($this->formatConfig($config))
+        );
+    }
+
+    /**
+     * Create an instance of the specified driver.
+     *
      * @return \Laravel\Socialite\Two\AbstractProvider
      */
     protected function createTwitterOAuth2Driver()
@@ -140,20 +158,6 @@ class SocialiteManager extends Manager implements Contracts\Factory
             $this->container->make('request'), $config['client_id'],
             $config['client_secret'], $this->formatRedirectUrl($config),
             Arr::get($config, 'guzzle', [])
-        );
-    }
-
-    /**
-     * Create an instance of the specified driver.
-     *
-     * @return \Laravel\Socialite\One\AbstractProvider
-     */
-    protected function createTwitterDriver()
-    {
-        $config = $this->config->get('services.twitter');
-
-        return new TwitterProvider(
-            $this->container->make('request'), new TwitterServer($this->formatConfig($config))
         );
     }
 
