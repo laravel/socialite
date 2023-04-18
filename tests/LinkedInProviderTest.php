34e286e146a43e093273996ed71e5ad918f9b2b8
Dries Vints <dries@vints.be>
2023-04-18 10:50:15 +0200

    Fix test

--- tests/LinkedInProviderTest.php
+++ tests/LinkedInProviderTest.php
@@ -10,6 +10,7 @@ use Laravel\Socialite\Two\User;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 
 class LinkedInProviderTest extends TestCase
 {
@@ -25,15 +26,24 @@ class LinkedInProviderTest extends TestCase
         $request = m::mock(Request::class);
         $request->allows('input')->with('code')->andReturns('fake-code');
 
+        $stream = m::mock(StreamInterface::class);
+        $stream->allows('__toString')->andReturns(json_encode(['access_token' => 'fake-token']));
+
         $accessTokenResponse = m::mock(ResponseInterface::class);
-        $accessTokenResponse->allows('getBody')->andReturns(json_encode(['access_token' => 'fake-token']));
+        $accessTokenResponse->allows('getBody')->andReturns($stream);
+
+        $basicProfileStream = m::mock(StreamInterface::class);
+        $basicProfileStream->allows('__toString')->andReturns(json_encode(['id' => $userId = 1]));
 
         $basicProfileResponse = m::mock(ResponseInterface::class);
-        $basicProfileResponse->allows('getBody')->andReturns(json_encode(['id' => $userId = 1]));
+        $basicProfileResponse->allows('getBody')->andReturns($basicProfileStream);
+
+        $emailAddressStream = m::mock(StreamInterface::class);
+        $emailAddressStream->allows('__toString')->andReturns(json_encode(['elements' => []]));
 
         // Make sure email address response contains no values.
         $emailAddressResponse = m::mock(ResponseInterface::class);
-        $emailAddressResponse->allows('getBody')->andReturns(json_encode(['elements' => []]));
+        $emailAddressResponse->allows('getBody')->andReturns($emailAddressStream);
 
         $guzzle = m::mock(Client::class);
         $guzzle->expects('post')->andReturns($accessTokenResponse);
