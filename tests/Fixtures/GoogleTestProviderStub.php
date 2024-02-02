a21a7da44cf27d5f5e55c24ae997081ad99cdddc
Connor Tumbleson <connor@sourcetoad.com>
2024-02-02 17:55:41 -0500

    test: assert refresh token works on Google refresh

---
+++ tests/Fixtures/GoogleTestProviderStub.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Laravel\Socialite\Tests\Fixtures;
+
+use Laravel\Socialite\Two\GoogleProvider;
+use Laravel\Socialite\Two\User;
+use Mockery as m;
+use stdClass;
+
+class GoogleTestProviderStub extends GoogleProvider
+{
+    /**
+     * @var \GuzzleHttp\Client|\Mockery\MockInterface
+     */
+    public $http;
+
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('http://auth.url', $state);
+    }
+
+    protected function getTokenUrl()
+    {
+        return 'http://token.url';
+    }
+
+    protected function getUserByToken($token)
+    {
+        return ['id' => 'foo'];
+    }
+
+    protected function mapUserToObject(array $user)
+    {
+        return (new User)->map(['id' => $user['id']]);
+    }
+
+    /**
+     * Get a fresh instance of the Guzzle HTTP client.
+     *
+     * @return \GuzzleHttp\Client|\Mockery\MockInterface
+     */
+    protected function getHttpClient()
+    {
+        if ($this->http) {
+            return $this->http;
+        }
+
+        return $this->http = m::mock(stdClass::class);
+    }
+}
