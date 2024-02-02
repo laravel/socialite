5e6e9963195593e45b34b1b6bd127bfc0a61b11f
Connor Tumbleson <connor@sourcetoad.com>
2024-02-02 17:43:36 -0500

    fix: default to known refresh token on Google refresh

--- src/Two/GoogleProvider.php
+++ src/Two/GoogleProvider.php
@@ -62,6 +62,21 @@ class GoogleProvider extends AbstractProvider implements ProviderInterface
     /**
      * {@inheritdoc}
      */
+    public function refreshToken($refreshToken)
+    {
+        $response = $this->getRefreshTokenResponse($refreshToken);
+
+        return new Token(
+            Arr::get($response, 'access_token'),
+            Arr::get($response, 'refresh_token', $refreshToken),
+            Arr::get($response, 'expires_in'),
+            explode($this->scopeSeparator, Arr::get($response, 'scope', ''))
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function mapUserToObject(array $user)
     {
         // Deprecated: Fields added to keep backwards compatibility in 4.0. These will be removed in 5.0
