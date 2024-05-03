0a780cc779a31c84c81534f8a3ac8ae679be9f33
Taylor Otwell <taylor@laravel.com>
2024-05-03 15:31:15 -0500

    formatting

--- src/Two/SlackOpenIdProvider.php
+++ src/Two/SlackOpenIdProvider.php
@@ -7,20 +7,39 @@ use Illuminate\Support\Arr;
 
 class SlackOpenIdProvider extends AbstractProvider implements ProviderInterface
 {
+    /**
+     * The scopes being requested.
+     *
+     * @var array
+     */
     protected $scopes = ['openid', 'email', 'profile'];
 
+    /**
+     * The separating character for the requested scopes.
+     *
+     * @var string
+     */
     protected $scopeSeparator = ' ';
 
+    /**
+     * {@inheritdoc}
+     */
     protected function getAuthUrl($state)
     {
         return $this->buildAuthUrlFromBase('https://slack.com/openid/connect/authorize', $state);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function getTokenUrl()
     {
         return 'https://slack.com/api/openid.connect.token';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function getUserByToken($token)
     {
         $response = $this->getHttpClient()->get('https://slack.com/api/openid.connect.userInfo', [
@@ -30,6 +49,9 @@ class SlackOpenIdProvider extends AbstractProvider implements ProviderInterface
         return json_decode($response->getBody(), true);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function mapUserToObject(array $user)
     {
         return (new User)->setRaw($user)->map([
