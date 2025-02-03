23b5dab69e0eab1306ae2d6f67bb9595a097eb7b
Ostap Brehin <osbre@protonmail.com>
2025-02-03 20:37:40 +0000

    Update facade PHPDoc

--- src/Facades/Socialite.php
+++ src/Facades/Socialite.php
@@ -7,7 +7,8 @@ use Laravel\Socialite\Contracts\Factory;
 
 /**
  * @method static \Laravel\Socialite\Contracts\Provider driver(string $driver = null)
- * @method static \Laravel\Socialite\Two\AbstractProvider buildProvider($provider, $config)
+ * @method static \Laravel\Socialite\Two\AbstractProvider buildProvider(string $provider, array $config)
+ * @method static \Laravel\Socialite\SocialiteManager extend(string $driver, \Closure $callback)
  * @method array getScopes()
  * @method \Laravel\Socialite\Contracts\Provider scopes(array|string $scopes)
  * @method \Laravel\Socialite\Contracts\Provider setScopes(array|string $scopes)
